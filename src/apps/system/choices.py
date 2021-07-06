3403630be1ba6efc5e49211c90346ac1b74c6b5e
wonseok-han <oshan1112@gmail.com>
2021-07-06 17:03:38 +0900

    YES/NO Choices 추가

--- src/apps/system/choices.py
+++ src/apps/system/choices.py
@@ -8,3 +8,12 @@ class UseYnChoices(TextChoices):
 
     사용 = ("Y", "사용")
     미사용 = ("N", "미사용")
+
+
+class YnChoices(TextChoices):
+    """
+    Y/N Choices
+    """
+
+    YES = ("Y", "Y")
+    NO = ("N", "N")
