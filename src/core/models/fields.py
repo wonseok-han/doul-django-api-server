2dc5843e54a4c845cd6d8541dfe4ee1762387c2a
wonseok-han <oshan1112@gmail.com>
2021-07-06 15:42:33 +0900

    정렬순서 PositiveIntegerField 추가

--- src/core/models/fields.py
+++ src/core/models/fields.py
@@ -1,4 +1,4 @@
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -16,3 +16,11 @@ class ChineseCharField(models.CharField):
     """
 
     default_validators = [RegexValidator(r"^[一-龥\d\s]*$", message="중문, 숫자만 입력해주세요.")]
+
+
+class OrderIntegerField(models.PositiveIntegerField):
+    """
+    정렬순서 필드 (1~100)
+    """
+
+    default_validators = [MinValueValidator(1), MaxValueValidator(100)]
