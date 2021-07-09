5b02d4c720cdb022eb065960cf48c76456dbb712
wonseok-han <oshan1112@gmail.com>
2021-07-09 10:25:24 +0900

    편의성을 위한 Shell Script 작성

---
+++ run.sh
@@ -0,0 +1,3 @@
+# TODO: 추후에 docker-compose를 이용한 gunicorn 구동을 해볼 수 있지 않을까?
+# runserver 구동
+python manage.py runserver
---
+++ runner/call_api.sh
@@ -0,0 +1,3 @@
+# Frontend 화면 개발이 되지 않았을 경우 애라의 Shell Script나 Postman, Insomnia 등의 도구를 통해 API를 확인할 수 있습니다.
+# host 뒤에 API를 확인하고자하는 Sub Url, QueryString을 추가해주세요.
+curl http://localhost:8000/system/common_code/system_common_code_master/
