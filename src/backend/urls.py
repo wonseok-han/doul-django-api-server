f4e4047819e6a47910f9cb17e3c61796ba523ab1
wonseok-han <oshan1112@gmail.com>
2021-07-12 10:45:44 +0900

    Transaction 처리 기본 API 추가

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("core/", include("core.urls")),
     path("system/", include("apps.system.urls")),
 ]
 
