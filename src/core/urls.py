fd7f3806ce2c8b31d69f0cd50eeb1b1ec0b30256
wonseok-han <oshan1112@gmail.com>
2021-07-12 11:24:11 +0900

    core/batch/의 url 정의 형식 변경

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 from core import views
 
-
-router = DefaultRouter()
-router.register("batch", views.batch, name="batch")
+app_name = "core"
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("batch/", views.batch, name="batch"),
 ]
