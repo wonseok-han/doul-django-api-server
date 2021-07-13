60abd21f4846661510e16ab2cdde955960b23613
wonseok-han <oshan1112@gmail.com>
2021-07-13 09:08:21 +0900

    공통코드Detail 리스트 조회 API 조회조건 추가

--- src/apps/system/common_code/views/views.py
+++ src/apps/system/common_code/views/views.py
@@ -1,5 +1,6 @@
 from apps.system.models import SystemCommonCodeMaster, SystemCommonCodeDetail
 from core.mixins import CoreMixin
+from django.http import Http404
 from rest_framework.viewsets import ModelViewSet
 from ..serializers import (
     SystemCommonCodeMasterSerializer,
@@ -15,3 +16,24 @@ class SystemCommonCodeMasterViewSet(CoreMixin, ModelViewSet):
 class SystemCommonCodeDetailViewSet(CoreMixin, ModelViewSet):
     queryset = SystemCommonCodeDetail.objects.all()
     serializer_class = SystemCommonCodeDetailSerializer
+
+    search_fields = ["common_cd_key"]
+
+    def list(self, request, *args, **kwargs):
+        """
+        리스트를 조회합니다.
+        """
+        queryset = self.get_queryset()
+
+        common_cd_key = request.query_params.get("common_cd_key")
+        if common_cd_key is None:
+            raise Http404("공통코드 쿼리스트링이 누락되었습니다.")
+
+        conditions = {}
+        for field_name, value in self.request.query_params.items():
+            if field_name in self.search_fields:
+                conditions[field_name] = value
+
+        queryset = queryset.filter(**conditions)
+
+        return super().list(request, queryset=queryset)
